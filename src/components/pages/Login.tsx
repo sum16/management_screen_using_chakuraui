d097a34a586f980223229550ac8649938f01a85c
yasuyuki sato <66477859+sum16@users.noreply.github.com>
2021-09-30 21:36:49 +0900

    initial commit

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,6 +1,31 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Input,
+  Stack
+} from "@chakra-ui/react";
 import { memo, VFC } from "react";
 // React.memoを使うと親から子コンポーネントに渡している props が更新されない限り(後述する callback 関数とかは別)子コンポーネントは再レンダリングされない
 
 export const Login: VFC = memo(() => {
-  return <p>ログインページ</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザー管理画面
+        </Heading>
+        <Divider my={4} />
+        {/* Stack ~囲った要素を問等間隔で配置 */}
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="ユーザーID" />
+          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+            ログイン
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
